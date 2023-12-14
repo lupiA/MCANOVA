8137b5e85735ad3ff9b4b04dc44e3ee02e77c1d5
lupiA <42622030+lupiA@users.noreply.github.com>
2023-12-14 14:43:44 -0500

    Update PGS_portability_app.R

--- R/PGS_portability_app.R
+++ R/PGS_portability_app.R
@@ -316,7 +316,7 @@ PGS_portability_app <- function() {
             theme.ggplot
         }
       }
-    })
+    }, height=function() { session$clientdata$output_histogram_width * 1.0 })
     
     output$download_table <- downloadHandler(
       filename = function() {
